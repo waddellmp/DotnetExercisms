5a21eac9bd342b963e8b0cfb160bd549a247ac17
Matthew Waddell <waddellmp08@gmail.com>
2025-02-03 10:26:03 -0600

    Add string reverse method and test

---
+++ ExercismsLibrary/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ExercismsLibray
+{
+    public static class ReverseString
+    {
+        public static string Reverse(string input)
+        {
+            if (String.IsNullOrEmpty(input)) return "";
+
+            StringBuilder sb = new();
+            
+            // Loop through string from end -> start
+            for(int i = input.Length - 1; i > -1; i--)
+            {
+                sb.Append(input[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ExercismsTests/ReverseStringTest.cs
@@ -0,0 +1,24 @@
+﻿using ExercismsLibray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercismsTests
+{
+    public class ReverseStringTest
+    {
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData("", "")]
+        public void ReverseStringSuccess(string input, string expected)
+        {
+            // Act
+            var actual = ReverseString.Reverse(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}

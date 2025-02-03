fee418406b3b5afa0172a458bc1509c6a25a517b
Matthew <waddellmp08@gmail.com>
2025-02-02 22:14:42 -0600

    Convert to primary ctor

--- ExercismsLibrary/BirdCount.cs
+++ ExercismsLibrary/BirdCount.cs
@@ -1,17 +1,14 @@
 ﻿namespace ExercismsLibrary;
 
-public class BirdCount
+public class BirdCount(int[] birdsPerDay)
 {
-    private readonly int[] _birdsPerDay;
-    
     private static readonly int[] BirdsLastWeek = new int[]{0, 2, 5, 3, 7, 8, 4};
-    
-    public BirdCount(int[] birdsPerDay) => this._birdsPerDay = birdsPerDay;
+
     public static int[] LastWeek() => BirdsLastWeek;
     
-    public int Today() => this._birdsPerDay.LastOrDefault(0);
-    public void IncrementTodaysCount() => this._birdsPerDay[6]++;
-    public bool HasDayWithoutBirds() => this._birdsPerDay.Any(x => x == 0);
-    public int CountForFirstDays(int numberOfDays) => this._birdsPerDay.Take(numberOfDays).Sum();
-    public int BusyDays() => this._birdsPerDay.Count(x => x >= 5);
+    public int Today() => birdsPerDay.LastOrDefault(0);
+    public void IncrementTodaysCount() => birdsPerDay[6]++;
+    public bool HasDayWithoutBirds() => birdsPerDay.Any(x => x == 0);
+    public int CountForFirstDays(int numberOfDays) => birdsPerDay.Take(numberOfDays).Sum();
+    public int BusyDays() => birdsPerDay.Count(x => x >= 5);
 }
